a32607b8255efb9fe37823c7171747727a80b92c
Gesselle Villaruel <gessellevillaruel@gmail.com>
2022-03-31 10:59:35 +0800

    Added route for side bar menu scholarship sponsor

--- resources/views/System_admin/add_new_record.blade.php
+++ resources/views/System_admin/add_new_record.blade.php
@@ -43,7 +43,7 @@
                             <a href="#"><span class="fa fa-briefcase mr-3"></span> Job Opportunity</a>
                         </li>
                         <li>
-                            <a href="#"><span class="fa fa-cloud-upload mr-3"></span>Scholarship Sponsors</a>
+                            <a href="scholarship_sponsors"><span class="fa fa-cloud-upload mr-3"></span>Scholarship Sponsors</a>
                         </li>
                         <li>
                             <a href="#"><span class="fa fa-paper-plane mr-3"></span> Email</a>
